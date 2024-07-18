75b6fdba087af46260c34b9454a27a4782d98c03
wathika-eng <wathika02@gmail.com>
2024-07-19 01:17:04 +0300

    fix_dirs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
 	plugins: [react()],
 	server: {
 		proxy: {
-			'/api/': {
+			'/advocates': {
 				target: 'http://localhost:8000/api/',
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, ''),
